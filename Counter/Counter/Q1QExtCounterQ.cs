f8ec0a890f59f218574fd38c66076d2f81e5f084
Adheli Dos Santos Tavares <A00277752@student.ait.ie>
2019-10-23 19:51:59 +0100

    LinQ class

---
+++ Counter/Counter/Q1QExtCounterQ.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    static class Functions
+    {
+        public static void Decrement(this Counter c)
+        {
+            c.Count--;
+        }
+    }
+
+    class Counter
+    {
+        public int Count { get; set; }
+        // automatic property for var "int count"
+        public void Increment() { Count++; }
+        public int ReadValue() { return Count; }
+    }
+
+    // insert extension method for "void decrement()
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 0;
+            Counter c = new Counter { Count = 1 };
+            	//Define Counter object c using object initializer to set count=0
+
+            while (option != 4)
+            {
+                Console.WriteLine("\nChoose Option\n");
+                Console.WriteLine("================\n");
+                Console.WriteLine("1. Increment");
+                Console.WriteLine("2. Decrement");
+                Console.WriteLine("3. Read Value");
+                Console.WriteLine("4. Exit");
+
+                option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    c.Increment();
+
+                }
+                else if (option == 2)
+                {
+                    c.Decrement();
+                }
+                else if (option == 3)
+                {
+                    int res = c.ReadValue();
+                    Console.WriteLine("Val="+res);
+                }
+
+                else if (option == 4)
+                {
+                    Console.WriteLine("Exit");
+                }
+            }
+        }
+    }
+}
---
+++ MyCountQ4/MyCountQ4/Q4LinQHOFMyCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+delegate bool GreaterThan(int v);
+
+static class Functions
+{
+    public static int MyCount(this int[] ints)
+    {
+        return ints.Length;
+    }
+
+    public static int MyCount(this int[] ints, GreaterThan func)
+    {
+        int count = 0;
+        foreach (int value in ints)
+        { if (func(value)) { count++; } }
+
+        return count;
+    }
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] a = { 3, 3, 5, 8, 9, 1 };
+        int result = a.MyCount(e => e % 2 == 0);
+        Console.WriteLine("Res={0}", result);
+        Console.ReadLine();
+    }
+}
